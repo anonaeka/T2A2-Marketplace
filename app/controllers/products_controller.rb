66861bbba94f9c3abc15c1ceedefcf8c9178d78a
anonaeka <anonvisualdesign@gmail.com>
2021-05-18 13:05:26 +1000

    activestorage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -67,6 +67,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :description, :price)
+      params.require(:product).permit(:name, :description, :price, :image)
     end
 end

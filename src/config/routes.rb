54f2eaf7865284631d1c722f9d70949179dd8519
anonaeka <anonvisualdesign@gmail.com>
2021-05-23 16:14:57 +1000

    userpart

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :v_space, :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'v_space#index'
-  # get '/search' => 'products#search', :as => 'search_products'
+  
 end
--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(
+    {
+        name: "TestSeed",
+        description: "Okay Number One Okay Na Ja Na Ja Bok La Suk Noi Na",
+        price: "50",
+    }
+)
